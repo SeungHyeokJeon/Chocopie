0d97582f6a373ffccae6367cb7bbf82f48c6213e
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-05-07 21:16:43 +0900

    Kakao Login 기능 추가

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,7 @@ app_name = 'account'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('loginKakao/', views.loginKakao, name='loginKakao'),
+    path('loginRedirectKakao/', views.loginRedirectKakao, name='loginRedirectKakao'),
+    path('logoutKakao/', views.logoutKakao, name='logoutKakao'),
 ]
