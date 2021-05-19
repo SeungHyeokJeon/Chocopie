22d6974b312a03483f5a1f5450a4e94f99c42b6c
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-05-19 10:23:32 +0900

    주석 추가

--- board/views.py
+++ board/views.py
@@ -6,17 +6,16 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def test(request):
-    #data = Stores.objects.all()
-    data = list(range(1,30))
+    data = Stores.objects.all() # db 전체 값 가져오기
+    
+    listLength = 5  # 한번에 불러올 게시글 개수
+    totalPage = math.ceil(len(data)/listLength) # 전체 페이지 계산
 
-    listLength = 5
-    totalPage = math.ceil(len(data)/listLength)
-
-    paginator = Paginator(data,listLength)
-    page = request.GET.get('page')
+    paginator = Paginator(data,listLength) # 게시글 나누기
+    page = request.GET.get('page') # page 파라미터 있으면 갖고오기
     
     try:
-        post_list=paginator.page(page)
+        post_list=paginator.page(page) # 현재 페이지 지정
     except PageNotAnInteger:
         post_list=paginator.page(1)
     except EmptyPage:
@@ -27,8 +26,7 @@ def test(request):
 
 # 추가될 요소 접근
 def test_ajax(request):
-    #data = Stores.objects.all()
-    data = list(range(1,30))
+    data = Stores.objects.all()
 
     listLength = 5
     totalPage = math.ceil(len(data)/listLength)
