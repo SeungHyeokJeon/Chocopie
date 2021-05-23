5ed037980a94af3bf502d434e6f5b8a7b7e8eba0
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-05-23 18:26:17 +0900

    회원가입 버그 수정

--- mainpage/views.py
+++ mainpage/views.py
@@ -11,18 +11,18 @@ def mainpage(request):
     
     if authId!='' and authId!=None: # 로그인된 계정이 존재하면
         # 로그인계정의 회원정보가 등록되어있는지 판단
-        existId = Userinfo.objects.filter(id=int(authId))
-        #existId = Userinfo.objects.all()
-        if existId.exists():
+        existId = Userinfo.objects.filter(id=authId)
+        if not existId.exists():
             # 등록이 안되어 있으면 넘겨줄 값들을 하나씩 불러옴
             # 넘겨주는 값: auth_user.id, provider, name
-            getSocialAccount = SocialAccount.objects.filter(user_id=authId)
-            extraData = json.loads(getSocialAccount[0].extra_data)
-            provider = getSocialAccount[0].provider # provider
+            socialAccountInfo = SocialAccount.objects.get(user_id=authId)
+            
+            provider = socialAccountInfo.provider # provider
+
             if provider=='kakao': # provider가 kakao면 nickname으로 name 가져옴
-                name = extraData['properties']['nickname']
+                name = socialAccountInfo.extra_data['properties']['nickname']
             else: # provider가 google이나 naver면 name으로 가져옴
-                name = extraData['name']
+                name = socialAccountInfo.extra_data['name']
 
             context={'authid':authId, 'provider':provider,'name':name}
             return render(request, 'accounts/additionalRegister.html',context)
