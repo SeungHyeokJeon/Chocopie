58abe7cfa12179053295ec0f8472e46f8b3808fd
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-05-23 19:42:38 +0900

    회원탈퇴 기능 구현 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('withdrawal/', views.withdrawal, name='withdrawal'),
     path('loginKakao/', views.loginKakao, name='loginKakao'),
     path('register/',views.register, name='register'),
+    path('withdrawal/process/', views.withdrawalProcess, name='withdrawalProcess')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 
 from accounts.models import Userinfo
 from django.contrib.auth.models import User # auth_user
+from allauth.socialaccount.models import SocialAccount, SocialToken
+from allauth.account.models import EmailAddress
+from django.contrib.sessions.models import Session
 
 from datetime import datetime
 
@@ -12,9 +15,6 @@ def login(request):
     dataList = User.objects.filter(id=authId)
     context = {'userData':dataList}
     return render(request, 'accounts/login.html',context)
-
-def logout(request):
-    return render(request, 'accounts/logout.html')
 
 def loginKakao(request):
     restApiKey = 'cc4da3338c1087e45c43b48a355072be'
@@ -32,4 +32,34 @@ def register(request):
     userInstance = Userinfo(id=User.objects.get(id=authid), provider=provider, name=name, email=email, date_joined=datetime.now())
     userInstance.save()
 
+    return redirect(reverse('mainpage:mainpage'))
+
+def withdrawal(request):
+    return render(request, 'accounts/withdrawal.html')
+
+def withdrawalProcess(request):
+    authId = request.session.get('_auth_user_id')
+    
+    isDelete = request.POST.get('isdelete')
+    isDelete='True'
+    # 비정상 접근 막기
+    if isDelete=='True':
+        # 데이터가 삭제될 테이블: socialaccount_socialaccount, socialaccount_socialtoken, account_emailaddress, auth_user
+        infoInstance = Userinfo.objects.filter(id=13)
+        
+        socialAccountInstance = SocialAccount.objects.get(user_id=authId)
+        # token 삭제 위한 socialaccount id값
+        socialAccountId = socialAccountInstance.id
+
+        socialTokenInstance = SocialToken.objects.get(id=socialAccountId)
+        emailAddressInstance = EmailAddress.objects.get(user_id=authId)
+        userInstance = User.objects.get(id=authId)
+
+        socialTokenInstance.delete()
+        socialAccountInstance.delete()
+        emailAddressInstance.delete()
+        userInstance.delete()
+
+        # 현재 세션 삭제
+        request.session.flush()
     return redirect(reverse('mainpage:mainpage'))
