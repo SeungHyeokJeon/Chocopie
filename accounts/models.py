10be98d20fb22fcc80e6b9a18ea03dcb1cc87dd6
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-05-31 20:57:19 +0900

    model 수정

--- accounts/models.py
+++ accounts/models.py
@@ -4,11 +4,27 @@ from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 
 # Create your models here.
+class User(User):
+    pass
+class EmailAddress(EmailAddress):
+    pass
+class SocialAccount(SocialAccount):
+    pass
+class SocialApp(SocialApp):
+    pass
+class SocialToken(SocialToken):
+    pass
+
+
 class Userinfo(models.Model):
     id = models.OneToOneField(User, models.CASCADE, db_column='id', primary_key=True)
     provider = models.CharField(max_length=30, blank=True, null=True)
-    name = models.CharField(db_column='NAME', max_length=64, blank=True, null=True)  # Field name made lowercase.
-    email = models.CharField(max_length=64, blank=True, null=True)
+    name = models.CharField(db_column='NAME', max_length=256, blank=True, null=True)  # Field name made lowercase.
+    gender = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=256, blank=True, null=True)
+    address = models.CharField(max_length=1024, blank=True, null=True)
     date_joined = models.DateTimeField(blank=True, null=True)
 
     class Meta:
