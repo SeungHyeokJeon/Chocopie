efa338915c4f983ed34684f231cb6084d8e546da
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-06-13 16:28:58 +0900

    댓글 보기 버튼 수정

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 import requests
 from random import randint
 
+# Create your models here.
 class PhoneVerification(models.Model):
     user  = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     phone = models.CharField(unique=True, max_length=11)
@@ -13,7 +14,8 @@ class PhoneVerification(models.Model):
     class Meta:
         db_table = 'phone_verifications'
 
-# Create your models here.
+def get_default_something():
+    return {'accept_list': [], 'reject_list': []}
 class Userinfo(models.Model):
     id = models.OneToOneField(User, models.CASCADE, db_column='id', primary_key=True)
     provider = models.CharField(max_length=30, blank=True, null=True)
@@ -25,6 +27,7 @@ class Userinfo(models.Model):
     address = models.CharField(max_length=1024, blank=True, null=True)
     date_joined = models.DateTimeField(blank=True, null=True)
     like_store = models.CharField(max_length=30, blank=True, null=True)
+    shopping_basket = models.JSONField(default=get_default_something, blank=True, null=True)
 
     class Meta:
         managed = True
